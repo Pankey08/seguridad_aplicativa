6b8565e255851167dead0f42c5d5c2bc85d560a8
Pankey08 <56138882+Pankey08@users.noreply.github.com>
2023-08-28 12:49:26 -0600

    Create TablaMultiplicar.py

---
+++ Día/TablaMultiplicar.py
@@ -0,0 +1,13 @@
+#tabla de multiplicar
+
+eMultiplicador = 1
+eNumero = 1
+eResultado = 0
+
+while (eMultiplicador <= 10):
+    
+    for eNumero in range (11):
+        
+        print(eNumero, "x", eMultiplicador, "=", eNumero * eMultiplicador)
+        
+    eMultiplicador += 1
