673d98d885f8dd907d849265fec7f765ce5e72cd
Pankey08 <56138882+Pankey08@users.noreply.github.com>
2023-08-29 13:27:50 -0600

    Update and rename Actividad2 to Actividad2.sh

---
+++ Día2/Actividad2.sh
@@ -0,0 +1,1 @@
+sudo useradd $1 -d /home/$1 -s /usr/bin/zsh -c "act2" -p "$6$AyDzqZRfvXovOqE5$rBuPptZQb14VaGsxYYvespqOLrsHdIszbZntu96Rq5zCpBaDbw/aD6ngcQ4J/EPeOwiagSiyn3DvktKrFfbqw1" && echo "se creo el usuario";
